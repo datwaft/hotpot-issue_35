1df89e431c746bcb344c95f508885c74570bb70b
David Guevara <datwaftx@gmail.com>
2021-09-13 20:53:12 -0600

    feat: do not print msg on success

--- config/init.lua
+++ config/init.lua
@@ -1,7 +1,6 @@
 local status, msg = pcall(function() require("utf8") end)
 if status then
     print("test result:", "SUCCESS")
-    print(msg)
 else
     print("test result:", "FAILED")
     print(msg)
